d8845886308b198bdbe0b2f31aad78589f27bf29
PfandBoss <68470553+PfandBoss@users.noreply.github.com>
2023-02-06 17:03:03 +0100

    added infotext

--- Assets/Scenes/Lara_Test/Door.cs
+++ Assets/Scenes/Lara_Test/Door.cs
@@ -8,6 +8,7 @@ public class Door : MonoBehaviour
 
     [SerializeField] private GameObject key;
     [SerializeField] private bool open = false;
+    [SerializeField] private GameObject infoText;
 
     public void Interact()
     {
@@ -16,6 +17,11 @@ public class Door : MonoBehaviour
         if (key == null | key.GetComponent<Item>().GetHolding())
         {
             OpenDoor();
+        }
+        else
+        {
+            infoText.SetActive(true);
+            Invoke(nameof(resetInfo),1.0f);
         }
     }
 
@@ -36,5 +42,10 @@ public class Door : MonoBehaviour
         Gizmos.DrawSphere(new Vector3(-0.6f, 1f, 0f), 0.2f);
         Gizmos.matrix = Matrix4x4.identity;
         Gizmos.DrawLine(gameObject.transform.position + Vector3.up, key.transform.position);
+    }
+    
+    private void resetInfo()
+    {
+        infoText.SetActive(false);
     }
 }
